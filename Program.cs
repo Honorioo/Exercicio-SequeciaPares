deacd3b2da984ce7f8ac0eeca38cf28316eb5551
Vinicius <viniciusgomeshonorio@hotmail.com>
2021-06-06 19:36:47 -0300

    Exercicio Sequencia Pares

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercicio_SequeciaPares_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pares = 0, numeros;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Números pares entre 0 e ? ");
+            numeros = int.Parse(Console.ReadLine());
+            Console.ResetColor();
+
+            while (pares <= numeros)
+            {
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.Write($"{ pares } ");
+                pares = pares + 2;
+            Console.ResetColor();
+            }
+        }
+    }
+}
